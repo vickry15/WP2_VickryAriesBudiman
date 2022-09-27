a7c7fc0e93d61e740242f5adb0ae7f1fdc1ace27
Vickry Aries Budiman <vickrybudiman2001@gmail.com>
2022-09-27 20:02:49 +0700

    add contoh1,contoh2,contoh3

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,10 +1,11 @@
 <?php
 
-class Contoh extends CI_Controller {
-    
+class Contoh1 extends CI_Controller {
+
 	public function index()
 	{
 		echo "Perkenalkan";
-        echo "Nama Saya Vickry Aries Budiman";
+        echo "Nama Saya Vickry Aries Budiman saya tinggal di cikarang saya NGABERS WKWK ";
 	}
 }
+    
